e8666ff00d9d0d821457f1c7b5c1f09dce37a9a6
Nurlord <352933nurzat@gmail.com>
2025-06-11 11:41:03 +0500

    add base routes and file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,28 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', component '},
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/notfound/notfound.component').then(
+        (m) => m.NotfoundComponent,
+      ),
+  },
 ];
