4a964c54d03f049cd8384449a7af9dd1cff2c63e
Nurlord <352933nurzat@gmail.com>
2025-06-12 20:59:02 +0500

    minor fixes

--- src/app/pages/cart-sidebar/cart-sidebar.component.ts
+++ src/app/pages/cart-sidebar/cart-sidebar.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { CartCardComponent } from "../../shared/common-ui/cart-card/cart-card.component";
+import { CartCardComponent } from '../../shared/common-ui/cart-card/cart-card.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-sidebar',
-  standalone: true,
   imports: [CartCardComponent, CommonModule],
   templateUrl: './cart-sidebar.component.html',
 })
@@ -22,12 +21,18 @@ export class CartSidebarComponent {
 
   // Подсчет общего количества товаров
   getTotalQuantity(): number {
-    return this.products.reduce((total, product) => total + (product.quantity || 1), 0);
+    return this.products.reduce(
+      (total, product) => total + (product.quantity || 1),
+      0,
+    );
   }
 
   // Подсчет итоговой суммы
   getTotalPrice(): number {
-    return this.products.reduce((total, product) => total + (product.price * (product.quantity || 1)), 0);
+    return this.products.reduce(
+      (total, product) => total + product.price * (product.quantity || 1),
+      0,
+    );
   }
 
   // Удаление продукта из корзины
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
 import { CategoryMenuComponent } from '../../shared/common-ui/category-menu/category-menu.component';
+import { CartSidebarComponent } from '../cart-sidebar/cart-sidebar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [CategoryMenuComponent],
+  imports: [CategoryMenuComponent, CartSidebarComponent],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  products = [
+    {
+      name: 'Apple 18W USB-C Power Adapter USB Type-C белый',
+      price: 6900,
+      image:
+        'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MU7T2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1542404784163',
+      quantity: 1,
+    },
+    {
+      name: 'Xiaomi Mi Power Bank 3 10000mAh',
+      price: 11900,
+      image: 'https://s.a-5.ru/i/file/161/7/64/37/643771dda7708123.jpg',
+      quantity: 1,
+    },
+    {
+      name: 'Samsung Galaxy Buds2 Pro Graphite',
+      price: 54900,
+      image:
+        'https://appmistore.ru/upload/iblock/b91/zbqxiyzgss4apa3no1ofmc27ev90v6rk.webp',
+      quantity: 1,
+    },
+    {
+      name: 'Xiaomi Mi Power Bank 3 10000mAh',
+      price: 11900,
+      image: 'https://s.a-5.ru/i/file/161/7/64/37/643771dda7708123.jpg',
+      quantity: 1,
+    },
+    {
+      name: 'Samsung Galaxy Buds2 Pro Graphite',
+      price: 54900,
+      image:
+        'https://appmistore.ru/upload/iblock/b91/zbqxiyzgss4apa3no1ofmc27ev90v6rk.webp',
+      quantity: 1,
+    },
+    {
+      name: 'Xiaomi Mi Power Bank 3 10000mAh',
+      price: 11900,
+      image: 'https://s.a-5.ru/i/file/161/7/64/37/643771dda7708123.jpg',
+      quantity: 1,
+    },
+    {
+      name: 'Samsung Galaxy Buds2 Pro Graphite',
+      price: 54900,
+      image:
+        'https://appmistore.ru/upload/iblock/b91/zbqxiyzgss4apa3no1ofmc27ev90v6rk.webp',
+      quantity: 1,
+    },
+  ];
+}
