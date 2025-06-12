8ca827a7af6d33d3308973353c09b22e80d8b426
Vintohlet <ruslantuleuoff@gmail.com>
2025-06-12 20:46:24 +0500

    fix: added header, footer

--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -7,13 +7,13 @@ import { FooterComponent } from './../../../pages/footer/footer.component';
   selector: 'app-main-layout',
   imports: [RouterOutlet, FooterComponent, HeaderComponent],
   template: `
-    <header>Header content</header>
+    <app-header/>
     <main class="flex justify-center items-center min-h-[95svh] mt-5 bg-[#EEEEEE]">
   <div class="w-full max-w-[1440px] px-4 sm:px-6 lg:px-8 xl:px-[360px]">
     <router-outlet></router-outlet>
   </div>
 </main>
-    <footer>Footer content</footer>
+    <app-footer/>
   `,
 })
 export class MainLayout {}
