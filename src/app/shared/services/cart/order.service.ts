94474322c10f2e4159aad5d0e8c1cd987bdba16c
Baqdaulet <Baqdaulet>
2025-06-20 10:20:20 +0500

    fixed product image removed private declaration

--- src/app/shared/services/cart/order.service.ts
+++ src/app/shared/services/cart/order.service.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  private readonly apiUrl = 'https://practiceapi.mooo.com/orders';
-
   constructor(private http: HttpClient) {}
 
   createOrder(items: { productId: string; storeId: string; quantity: number; price: number }[]): Observable<any> {
-    const token = localStorage.getItem('accessToken'); 
-    if (!token) {
-      throw new Error('Токен авторизации отсутствует. Пользователь не авторизован.');
-    }
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'accept': '*/*', 
-      Authorization: `Bearer ${token}`, 
-    });
-    const body = { items }; 
-    console.log('Request body:', body); 
-    return this.http.post(this.apiUrl, body, { headers });
+    const body = { items };
+    return this.http.post('/orders', body);
   }
 }
